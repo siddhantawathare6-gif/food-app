5741f5e69c06c28df107f5b553a9b3f231bfb0ea
siddhant <siddhantawathare6@gmail.com>
2026-09-04 14:23:57 +0530

    refactor restaurant packages

--- resturant/src/main/java/com/food/resturant/RestaurantNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.food.resturant;
-
-public class RestaurantNotFoundException extends RuntimeException {
-
-    public RestaurantNotFoundException(String msg) {
-        super(msg);
-    }
-
-}
--- resturant/src/main/java/com/food/resturant/repository/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.food.resturant.repository;
-
-import com.food.resturant.entity.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-}
